90a6585e58117033b657f07fbfdd7800d3a95e5d
Dani025a <dani025a@edu.easj.dk>
2024-05-03 15:01:33 +0200

    Added and tested the category flow with rabbitmq

---
+++ messages/ConsumeCategory.js
@@ -0,0 +1,56 @@
+import connectToRabbitMQ from './connection.js';
+import CategoryService from '../services/CategoryService.js';
+
+async function consumeCategoryEvents() {
+    try {
+        const connection = await connectToRabbitMQ();
+        const channel = await connection.createChannel();
+        const exchange = 'product';
+        const queue = 'category_events';
+
+        await channel.assertExchange(exchange, 'direct', {
+            durable: true
+        });
+
+        const assertQueue = await channel.assertQueue(queue, {
+            durable: true
+        });
+
+        channel.bindQueue(assertQueue.queue, exchange, 'category change');
+
+        console.log('Waiting for category events...');
+
+        channel.consume(assertQueue.queue, async (msg) => {
+            try {
+                const message = JSON.parse(msg.content.toString());
+                if (message.status === 'created') {
+                    console.log(message)
+
+                    const data = {
+                        _id: message.category.id,
+                        name: message.category.name,
+                        description: message.category.categoryDescription,
+                    }
+                    CategoryService.createCategory(data)
+                } else if (message.status === 'updated') {
+                    const data = {
+                        _id: message.category.id,
+                        name: message.category.name,
+                        description: message.category.categoryDescription,
+                    }
+                    await CategoryService.updateCategory(message.category.id, data);
+                } else if (message.status === 'deleted') {
+                    await CategoryService.deleteCategory(message.category.id);
+                }
+                console.log('Category event processed successfully');
+                channel.ack(msg)
+            } catch (error) {
+                console.error('Error processing category event:', error);
+            }
+        });
+    } catch (error) {
+        console.error('Error connecting to RabbitMQ:', error);
+    }
+}
+
+export default consumeCategoryEvents;
