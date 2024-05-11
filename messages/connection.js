79dfc5526401e8359938190dd843fb696a86e2e0
masp1234 <mart2c04@stud.kea.dk>
2024-05-11 17:21:09 +0200

    docker compose works here

--- messages/connection.js
+++ messages/connection.js
@@ -8,17 +8,26 @@ async function connectToRabbitMQ() {
         return connection;
     }
 
-    console.log(`Connecting to RabbitMQ...`);
+    const maxRetries = 6;
+    let retryCount = 0;
 
-    try {
-        connection = await amqp.connect(`amqp://${process.env.AMQP_HOST}`);
-    } catch (error) {
-        console.error('Error connecting to RabbitMQ:', error);
-        throw error;
+    while (retryCount < maxRetries) {
+        console.log(`Connecting to RabbitMQ (Attempt ${retryCount + 1} of ${maxRetries})...`);
+
+        try {
+            connection = await amqp.connect(`amqp://${process.env.AMQP_HOST}`);
+            console.log(`Connection to RabbitMQ established`);
+            return connection;
+        } catch (error) {
+            console.error('Error connecting to RabbitMQ:', error);
+            retryCount += 1;
+            const delaySeconds = Math.pow(2, retryCount);
+            console.log(`Retrying in ${delaySeconds} seconds...`);
+            await new Promise(resolve => setTimeout(resolve, delaySeconds * 1000));
+        }
     }
 
-    console.log(`Connection to RabbitMQ established`);
-    return connection;
+    throw new Error('Failed to connect to RabbitMQ after max retry attempts.');
 }
 
 export default connectToRabbitMQ;
